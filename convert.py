1cf03d1278c1c2ff7e58f071d0c7a4e577858c13
Michał Janiszewski <janisozaur+signed@gmail.com>
2020-08-18 00:05:23 +0200

    mux extension (incomplete)

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 import glob
 import subprocess
+import re
 
 files = glob.glob("images/**/*.png", recursive=True)
 processed = 0
@@ -12,6 +13,16 @@ for png_path in files:
     processed = processed + 1
     print(f'processing file {str(processed).rjust(pad_len)} of {count}, {str(int(processed * 100 / count)).rjust(3)}%: {png_path}')
     webp_path = png_path.replace(".png", ".webp")
-    subprocess.run(["cwebp", png_path, "-o", webp_path, "-lossless", "-m", "6"])
+    #subprocess.run(["cwebp", png_path, "-o", webp_path, "-lossless", "-m", "6"])
 
     # TODO: once converted the animation frames should be muxed
+
+webp_files = glob.glob("images/**/*.webp", recursive=True)
+regex = r"/(.*?)([+~-])([0-9]+)\.webp"
+to_mux = {}
+
+for webp_path in webp_files:
+    matches = re.finditer(regex, webp_path, re.MULTILINE)
+    for matchNum, match in enumerate(matches, start=1):
+        if to_mux[match.group(1)]:
+            to_mux[match.group(1)] = [].extend(to_mux[match.group(1)]).append(webp_path)
