c4ed2bd4dd6beab5c107e6d659bd0d19824219ee
Michał Janiszewski <janisozaur+signed@gmail.com>
2020-09-02 21:57:03 +0200

    Reduce logs

--- source/ImageBuffer.cpp
+++ source/ImageBuffer.cpp
@@ -375,7 +375,6 @@ namespace
 
 	std::vector<bool> ReadWEBP(const string& path, ImageBuffer& buffer, uint32_t frame)
 	{
-		printf("Trying to read webp image %s\n", path.c_str());
 		File file(path);
 		if (!file)
 			return {};
@@ -397,7 +396,6 @@ namespace
 			.size = file_size,
 		};
 
-		printf("creating demuxer\n");
 		auto webp_deleter = [](WebPDemuxer* demux) { WebPDemuxDelete(demux); };
 		std::unique_ptr<WebPDemuxer, decltype(webp_deleter)> demux(WebPDemux(&bitstream), webp_deleter);
 		uint32_t width = WebPDemuxGetI(demux.get(), WEBP_FF_CANVAS_WIDTH);
@@ -434,7 +432,7 @@ namespace
 
 				if (decoded == nullptr)
 				{
-					printf("Bad stuff happened!\n");
+					printf("Bad stuff happened! path = %s, frame = %u\n", path.c_str(), frame);
 					return result;
 				}
 
