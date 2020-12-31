c2b107497adee133e86e7bb282b0c8e7319c9aab
Thomas Ballinger <thomasballinger@gmail.com>
2020-12-31 13:09:56 -0500

    Experimental Netlify build

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# install scons
+python -m pip install scons
+
+# merge in emscripten changes
+git config --global user.email "example@example.com"
+git config --global user.name "beep boop"
+git fetch https://github.com/thomasballinger/endless-sky.git es-wasm-reformatted
+git merge FETCH_HEAD
+
+# build emscripten
+EMSCRIPTEN_VERSION="1.39.18"
+
+TEMPDIR=`mktemp -d`
+(cd ${TEMPDIR} &&
+ git clone https://github.com/emscripten-core/emsdk.git &&
+ cd emsdk &&
+ ./emsdk install ${EMSCRIPTEN_VERSION} &&
+ ./emsdk activate ${EMSCRIPTEN_VERSION}) || exit $?
+source ${TEMPDIR}/emsdk/emsdk_env.sh
+make output/index.html || exit $?
+rm -rf ${TEMPDIR}
